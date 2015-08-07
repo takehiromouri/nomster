64dfe3ae97bae351b9ff69ddc87939ff085f79b2
Takehiro Mouri <takehiromouri@gmail.com>
2015-08-07 09:54:48 +0000

    test without env

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,5 @@
 Geocoder.configure(
   :lookup => :bing,
-  :api_key => ENV['Ag7blIn-Vwq8066EkdXz64yADtQF1fKdw9gsQz4LB-mICtlP_13RbEt-C48aKsts'],
+  :api_key => 'Ag7blIn-Vwq8066EkdXz64yADtQF1fKdw9gsQz4LB-mICtlP_13RbEt-C48aKsts',
 )
 
