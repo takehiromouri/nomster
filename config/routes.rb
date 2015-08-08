67e90c997a88c7c8b0effd3465cdcb898330e5eb
Takehiro Mouri <takehiromouri@gmail.com>
2015-08-08 12:47:01 +0000

    First steps in adding in photo

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Nomster::Application.routes.draw do
   resources :places do
     resources :comments, :only => :create
   end
+
+  resources :photos do
+    resources :captions, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20150808123014_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+    	t.text :caption
+        t.timestamps
+
+        t.integer :place_id
+    end
+
+    add_index :photos, [:user_id, :place_id]
+    add_index :comments, :place_id
+  end
+end
